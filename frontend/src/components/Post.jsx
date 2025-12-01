bac3cd4de987159182aa67feaf6873ea33cf8c74
Adithya  Raj Melayikandy <adithyarajm@mac.mynetworksettings.com>
2025-12-01 16:28:48 -0500

    something

--- frontend/src/components/Post.jsx
+++ frontend/src/components/Post.jsx
@@ -5,7 +5,7 @@ export default function Post({ post, openPost }) {
   return (<div onClick={() => openPost(post.id)}>
     <li className="post-card">
       <div className="post-meta">
-        <Avatar avatarKey={post.avatarKey} avatarUrl={post.avatarUrl} size={36} username={post.username} userId={post.userId}/> <strong>{post.username}</strong>
+        <Avatar avatarKey={post.avatarKey} avatarUrl={post.avatarUrl } size={36} username={post.username} userId={post.userId}/> <strong>{post.username}</strong>
         <span className="post-time">{new Date(post.createdAt).toLocaleString()}</span>
       </div>
       <div className="post-body">{post.post}</div>
